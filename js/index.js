605335eb93e7eebb294791af7de1632d0d6f834a
reesharper <reestimharper@gmail.com>
2020-09-29 18:58:59 -0400

    finished 5 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,57 @@
-// Your code goes here
+
+//1
+const loginButton = document.querySelector('.btn');
+
+loginButton.addEventListener('click', (event) => {
+    window.open ("https://www.facebook.com/login", "Facebook_WindowName", "resizable, status");
+});
+
+
+
+//2
+const fullImage = document.querySelector('header img');
+
+fullImage.addEventListener('dblclick', (event) => {
+    window.open ("http://localhost:58496/fun-bus.927a34f2.jpg", "fun bus", "resizeable");
+});
+
+
+
+//3
+const imgBorder = document.querySelector('header img');
+
+imgBorder.addEventListener('mouseenter', (event) => {
+    event.target.style.border = "5px solid black"
+});
+
+
+//4
+imgBorder.addEventListener('mouseleave', (event) => {
+    event.target.style.border = "none"
+});
+
+
+
+//5
+function zoom(event) {
+    event.preventDefault();
+
+    scale += event.deltaY * -0.001;
+
+    scale = Math.min(Math.max(.125, scale), 4);
+
+    el.style.transform = `scale(${scale})`;
+}
+
+let scale = 1;
+const el = document.querySelector('.content-destination img');
+el.onwheel = zoom;
+
+
+
+//6
+document.addEventListener('keydown', (event) => {
+    if (event.key === '+') {
+        body.taget.style.color = 'red'
+    }
+})
